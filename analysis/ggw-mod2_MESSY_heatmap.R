dd49427c0dc865a08d8f925affb7c19e4f9a61c0
kgweisman <kweisman@stanford.edu>
2016-01-24 09:35:47 -0800

    heatmap stuff, etc.

--- analysis/ggw-mod2_MESSY_heatmap.R
+++ analysis/ggw-mod2_MESSY_heatmap.R
@@ -9,15 +9,65 @@
 
 library(RColorBrewer)
 library(gplots)
-heatmap.2(as.matrix(d4 %>% select(happy:pride)), 
-          col = brewer.pal(7, "RdYlBu"), 
-          Rowv = T, Colv = T, 
-          dendrogram = "none", 
-          trace = "none",
-          main = "Study 4",
-          labRow = d4$condition,
-          cexRow = 1.5,
-          cexCol = 1.5)
+
+temp_beetle <- d1 %>% 
+  filter(condition == "beetle") %>%
+  select(subid, happy:pride) %>%
+  gather(mc, score, -subid) %>%
+  mutate(score = as.integer(score) + 3) %>%
+  spread(mc, score) %>%
+  select(-subid) %>%
+  as.matrix()
+hm_beetle <- heatmap.2(temp_beetle,
+                       col = brewer.pal(7, "Blues"),
+                       Rowv = T, Colv = T,
+                       dendrogram = "none",
+                       trace = "none",
+                       main = "Study 1: Beetle",
+                       labRow = d1$condition,
+                       cexRow = 1.5,
+                       cexCol = 1.5)
+
+temp_robot <- d1 %>% 
+  filter(condition == "robot") %>%
+  select(subid, happy:pride) %>%
+  gather(mc, score, -subid) %>%
+  mutate(score = as.integer(score) + 3) %>%
+  spread(mc, score) %>%
+  select(-subid) %>%
+  as.matrix()
+hm_robot <- heatmap.2(temp_robot,
+                       col = brewer.pal(7, "Blues"),
+                       Rowv = T, Colv = T,
+                       dendrogram = "none",
+                       trace = "none",
+                       main = "Study 1: Robot",
+                       labRow = d1$condition,
+                       cexRow = 1.5,
+                       cexCol = 1.5)
+
+carpet_beetle <- data.frame(hm_beetle$carpet) %>%
+  add_rownames(var = "mc") %>%
+  gather(sub_index, response, -mc) %>%
+  add_rownames(var = "order") %>%
+  mutate(sub_index = factor(sub_index, labels = as.character(1:200)),
+         condition = "beetle")
+
+carpet_robot <- data.frame(hm_robot$carpet) %>%
+  add_rownames(var = "mc") %>%
+  gather(sub_index, response, -mc) %>%
+  add_rownames(var = "order") %>%
+  mutate(sub_index = factor(sub_index, labels = as.character(201:405)),
+         condition = "robot")
+
+carpet <- full_join(carpet_beetle, carpet_robot) %>%
+  arrange(condition, desc(order)) %>%
+  mutate(overall_order = 1:16200,
+         sub_index = reorder(sub_index, overall_order))
+
+ggplot(carpet, aes(x = mc, y = sub_index, fill = response)) + 
+  geom_tile() +
+  scale_fill_continuous()
 
 
 #### WITH GGPLOT2
